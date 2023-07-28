b796843c92f8b2a131d662dc239ca2fa086396dd
matheusboing <matboing@hotmail.com>
2023-07-27 22:13:35 -0300

    Inserido os registros de Categoria e Contato

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from contact import models
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_editable = 'phone',
+    list_display_links = 'id',
+    # list_per_page = 10,
+    # list_max_show_all = 200,
 
-# Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
