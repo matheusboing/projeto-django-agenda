80d9ec41696b097777d71f14481df4b91fc3494d
matheusboing <matboing@hotmail.com>
2023-07-31 11:49:15 -0300

    Alterado o caminho da view contact_view.py

---
+++ contact/views/__init__.py
@@ -0,0 +1,4 @@
+# Toda view nova, realizar o import dela dessa forma descrita abaixo.
+# Isso é uma forma de enganar o django, pois ele ainda assim lê o nome do package como 'views' e executa o __init__.py primeiro, sendo assim, a aplicação roda liso.
+
+from .contact_views import *
---
+++ contact/views/contact_views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404
+from contact.models import Contact
+
+def index(request):
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .order_by('-id')[:10]
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+    
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.get(pk=contact_id)
+    single_contact = get_object_or_404(
+        Contact,
+        pk=contact_id,
+        show=True)
+
+    context = {
+        'contact': single_contact,
+    }
+    
+    return render(
+        request,
+        'contact/contact.html',
+        context,
+    )
