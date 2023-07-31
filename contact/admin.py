b32b12c5c3d0ac4949851176e88ecfb637d473c6
matheusboing <matboing@hotmail.com>
2023-07-31 11:36:48 -0300

    Incluido show na administracao do django

--- contact/admin.py
+++ contact/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show'
     ordering = '-id',
     list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
-    list_editable = 'phone',
+    list_editable = 'phone', 'show'
     list_display_links = 'id',
     # list_per_page = 10,
     # list_max_show_all = 200,
