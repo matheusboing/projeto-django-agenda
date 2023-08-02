a649ac1ff46a5b6d9b6d3502175e62df3de5f2d4
matheusboing <matboing@hotmail.com>
2023-08-01 22:04:12 -0300

    Incluido os imports das views do contato

--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -2,3 +2,4 @@
 # Isso é uma forma de enganar o django, pois ele ainda assim lê o nome do package como 'views' e executa o __init__.py primeiro, sendo assim, a aplicação roda liso.
 
 from .contact_views import *
+from .contact_forms import *
