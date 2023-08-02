b5fb1c03f0917524700c17de97d3979f7e752136
matheusboing <matboing@hotmail.com>
2023-08-01 22:03:22 -0300

    Incluido as urls do CRUD

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,12 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update', views.update, name='update'),
+    path('contact/<int:contact_id>/delete', views.delete, name='delete'),
 ]
